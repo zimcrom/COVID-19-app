fa8fe31473391fa0ad2e58a000de80c24db4e371
Megan H <megannh6349@gmail.com>
2020-04-08 23:31:57 -0400

    updated small fixes to original skeleton

--- parallax-template/js/init.js
+++ parallax-template/js/init.js
@@ -1,8 +1,10 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+
+    $('#myModal').modal(options)
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
