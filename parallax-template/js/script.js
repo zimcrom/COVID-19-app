55100673de1324c3e2263cf45d93aeddfc18026a
mac <mac@macs-MacBook-Pro.local>
2020-04-09 21:10:27 -0400

    pseudocode

---
+++ parallax-template/js/script.js
@@ -0,0 +1,7 @@
+// in statistics tab once clicked will be taken to statistics html (Completed)
+// unload, statistics info printed in world statistics table 
+// unclick "choose your location button" 
+// modal pop up with zipcode input form"
+    // google map api will access location on input
+    // statistics will appear on the screen
+    
