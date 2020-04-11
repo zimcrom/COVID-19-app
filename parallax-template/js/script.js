664ac98302edc7a039377ab15d33db446ac77bd2
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-04-11 09:59:24 -0400

    test

--- parallax-template/js/script.js
+++ parallax-template/js/script.js
@@ -1,7 +1,7 @@
-// in statistics tab once clicked will be taken to statistics html (Completed)
-// unload, statistics info printed in world statistics table 
-// unclick "choose your location button" 
-// modal pop up with zipcode input form"
-    // google map api will access location on input
-    // statistics will appear on the screen
+// In statistics tab once clicked will be taken to statistics html (Completed)
+// On load, statistics info printed in world statistics table 
+// On click event listener "choose your location button" 
+// Modal pop up with zipcode input form
+    // Google map API will access location on input
+    // Statistics will appear on the screen
     
