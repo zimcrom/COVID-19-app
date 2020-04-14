27c7dfcac80c560db6f500c539b1b502559a1c3e
Reiner Bonsol <reinerbonsol93@gmail.com>
2020-04-13 23:06:51 -0400

    tried to add some script code but came up short. didnt really add much of anything

--- js/statistics.js
+++ js/statistics.js
@@ -1,68 +1,74 @@
 //STATISTICS TAB HTML PSEUDO
-
 // in statistics tab once clicked will be taken to statistics html (Completed)
 // On load,
-    // Create var for array of countries: 
-        // var countries = [
-        //     {
-        //         name: 'united-states',
-        //         status: [
-        //             'confirmed',
-        //             'active',
-        //             'deaths',
-        //             'recovered'
-        //         ]
-        //     },
-        //     {
-        //         name: 'italy',
-        //         status: [
-        //             'confirmed',
-        //             'active',
-        //             'deaths',
-        //             'recovered'
-        //         ]
-        //     },
-        //     {
-        //         name: 'spain',
-        //         status: [
-        //             'confirmed',
-        //             'active',
-        //             'deaths',
-        //             'recovered'
-        //         ]
-        //     },
-        //     {
-        //         name: 'germany',
-        //         status: [
-        //             'confirmed',
-        //             'active',
-        //             'deaths',
-        //             'recovered'
-        //         ]
-        //     },
-        //     {
-        //         name: 'china',
-        //         status: [
-        //             'confirmed',
-        //             'active',
-        //             'deaths',
-        //             'recovered'
-        //         ]
-        //     }
-        // ]
-        // for ( var i = 0; i < countries.length; i++)
-    //create var array for status: var status = ['confirmed', 'active', 'deaths', 'recovered']
-        // for (var i = 0; i < status.length; i++)
-    // grab world info by country total API endpoint
-        //API URL:var settings = {
-        //   "url": "https://api.covid19api.com/total/country/" + countries[i] + "/status/" + status[i] ,
-        //   "method": "GET",
-        //   "timeout": 0,
-        // };
+var countries = ['united-states', 'italy', 'spain', 'germany', 'china']
+var countryStatus = ['confirmed', 'active', 'deaths', 'recovered']
 
-        // $.ajax(settings).done(function (response) {
-        //   console.log(response);
-        // });
+for (var i = 0; i < countries.length; i++) {
+    console.log(countries[i]);
+}
+
+for (var j = 0; j < countryStatus.length; j++) {
+    console.log(countryStatus[j]);
+}
+
+function initStatPage() {
+    // Create var for array of objects in countries:
+
+    //Create a var that will go into countries array and get the countries names
+
+    // Create a var that will go into countries array and get the status
+    // Create for loop that will grab every name of the countries and append it
+    // for (var i = 0; i < countryName.length; i++) {
+
+    // }
+    var ajax1 = $.ajax({
+        url: 'https://api.covid19api.com/total/country/' + countries[0],
+        method: 'GET'
+    });
+
+    var ajax2 = $.ajax({
+        url: 'https://api.covid19api.com/total/country/' + countries[1],
+        method: 'GET'
+    });
+
+    var ajax3 = $.ajax({
+        url: 'https://api.covid19api.com/total/country/' + countries[2],
+        method: 'GET'
+    });
+
+    var ajax4 = $.ajax({
+        url: 'https://api.covid19api.com/total/country/' + countries[3],
+        method: 'GET'
+    });
+
+    var ajax5 = $.ajax({
+        url: 'https://api.covid19api.com/total/country/' + countries[4],
+        method: 'GET'
+    });
+
+    ajax1.then(function (response) {
+        console.log(response);
+    });
+
+    ajax2.then(function (response) {
+        console.log(response);
+    });
+}
+
+initStatPage();
+// var url = "https://api.covid19api.com/total/country/" + countryName;
+
+    // // grab world info by country total API endpoint
+    //     var settings2 = {
+    //       "url": "https://api.covid19api.com/total/country/" + countryName[i] + "/status/" + status[i] ,
+    //       "method": "GET",
+    //       "timeout": 0,
+    //     };
+
+    //     $.ajax(settings2).done(function (response) {
+    //       console.log(response);
+    //     });
 
         //$('<tr1>).append('<td>').text(response)
     // print to 'World Statistics' Table
@@ -77,3 +83,7 @@
     // var county = $(county).val()
 
     // applicationCache.gooele.https + county + apikey
+
+
+    // var tableRow = $('<tbody>').append('<tr>');
+        // var tableData = $(tableRow).append('<td>').text(countryName[i]);
